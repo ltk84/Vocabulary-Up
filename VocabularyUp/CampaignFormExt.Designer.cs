604ce7a7707fdf1dea1421d52726fb1ef88d3555
Truong Kim Lam <lamtk.84@gmail.com>
2020-11-04 20:12:16 +0700

    More UIs, continue fixing

---
+++ VocabularyUp/CampaignFormExt.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace VocabularyUp
+{
+    partial class CampaignFormExt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMainNav = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.pnlLib = new System.Windows.Forms.Panel();
+            this.pnlTab = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.pnlSearching = new System.Windows.Forms.Panel();
+            this.btnA = new Guna.UI2.WinForms.Guna2Button();
+            this.btnC = new Guna.UI2.WinForms.Guna2Button();
+            this.btnB = new Guna.UI2.WinForms.Guna2Button();
+            this.btnD = new Guna.UI2.WinForms.Guna2Button();
+            this.pnlMainNav.SuspendLayout();
+            this.pnlLib.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMainNav
+            // 
+            this.pnlMainNav.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.pnlMainNav.BorderRadius = 20;
+            this.pnlMainNav.Controls.Add(this.btnD);
+            this.pnlMainNav.Controls.Add(this.btnB);
+            this.pnlMainNav.Controls.Add(this.btnC);
+            this.pnlMainNav.Controls.Add(this.btnA);
+            this.pnlMainNav.Controls.Add(this.pnlLib);
+            this.pnlMainNav.Controls.Add(this.pnlSearching);
+            this.pnlMainNav.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMainNav.Location = new System.Drawing.Point(0, 0);
+            this.pnlMainNav.Name = "pnlMainNav";
+            this.pnlMainNav.ShadowDecoration.Parent = this.pnlMainNav;
+            this.pnlMainNav.Size = new System.Drawing.Size(1000, 520);
+            this.pnlMainNav.TabIndex = 14;
+            // 
+            // pnlLib
+            // 
+            this.pnlLib.BackColor = System.Drawing.Color.Transparent;
+            this.pnlLib.Controls.Add(this.pnlTab);
+            this.pnlLib.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlLib.Location = new System.Drawing.Point(54, 0);
+            this.pnlLib.Name = "pnlLib";
+            this.pnlLib.Size = new System.Drawing.Size(394, 520);
+            this.pnlLib.TabIndex = 9;
+            // 
+            // pnlTab
+            // 
+            this.pnlTab.BorderRadius = 20;
+            this.pnlTab.FillColor3 = System.Drawing.Color.Snow;
+            this.pnlTab.FillColor4 = System.Drawing.Color.Snow;
+            this.pnlTab.Location = new System.Drawing.Point(0, 45);
+            this.pnlTab.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnlTab.Name = "pnlTab";
+            this.pnlTab.ShadowDecoration.Parent = this.pnlTab;
+            this.pnlTab.Size = new System.Drawing.Size(394, 430);
+            this.pnlTab.TabIndex = 0;
+            // 
+            // pnlSearching
+            // 
+            this.pnlSearching.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlSearching.Location = new System.Drawing.Point(0, 0);
+            this.pnlSearching.Name = "pnlSearching";
+            this.pnlSearching.Size = new System.Drawing.Size(54, 520);
+            this.pnlSearching.TabIndex = 0;
+            // 
+            // btnA
+            // 
+            this.btnA.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnA.BackColor = System.Drawing.Color.Transparent;
+            this.btnA.BorderRadius = 15;
+            this.btnA.CheckedState.Parent = this.btnA;
+            this.btnA.CustomImages.Parent = this.btnA;
+            this.btnA.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnA.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnA.ForeColor = System.Drawing.Color.White;
+            this.btnA.HoverState.Parent = this.btnA;
+            this.btnA.Location = new System.Drawing.Point(551, 153);
+            this.btnA.Name = "btnA";
+            this.btnA.ShadowDecoration.Parent = this.btnA;
+            this.btnA.Size = new System.Drawing.Size(180, 72);
+            this.btnA.TabIndex = 10;
+            // 
+            // btnC
+            // 
+            this.btnC.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnC.BackColor = System.Drawing.Color.Transparent;
+            this.btnC.BorderRadius = 15;
+            this.btnC.CheckedState.Parent = this.btnC;
+            this.btnC.CustomImages.Parent = this.btnC;
+            this.btnC.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnC.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnC.ForeColor = System.Drawing.Color.White;
+            this.btnC.HoverState.Parent = this.btnC;
+            this.btnC.Location = new System.Drawing.Point(551, 250);
+            this.btnC.Name = "btnC";
+            this.btnC.ShadowDecoration.Parent = this.btnC;
+            this.btnC.Size = new System.Drawing.Size(180, 72);
+            this.btnC.TabIndex = 11;
+            // 
+            // btnB
+            // 
+            this.btnB.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnB.BackColor = System.Drawing.Color.Transparent;
+            this.btnB.BorderRadius = 15;
+            this.btnB.CheckedState.Parent = this.btnB;
+            this.btnB.CustomImages.Parent = this.btnB;
+            this.btnB.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnB.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnB.ForeColor = System.Drawing.Color.White;
+            this.btnB.HoverState.Parent = this.btnB;
+            this.btnB.Location = new System.Drawing.Point(749, 153);
+            this.btnB.Name = "btnB";
+            this.btnB.ShadowDecoration.Parent = this.btnB;
+            this.btnB.Size = new System.Drawing.Size(180, 72);
+            this.btnB.TabIndex = 12;
+            // 
+            // btnD
+            // 
+            this.btnD.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnD.BackColor = System.Drawing.Color.Transparent;
+            this.btnD.BorderRadius = 15;
+            this.btnD.CheckedState.Parent = this.btnD;
+            this.btnD.CustomImages.Parent = this.btnD;
+            this.btnD.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnD.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnD.ForeColor = System.Drawing.Color.White;
+            this.btnD.HoverState.Parent = this.btnD;
+            this.btnD.Location = new System.Drawing.Point(749, 250);
+            this.btnD.Name = "btnD";
+            this.btnD.ShadowDecoration.Parent = this.btnD;
+            this.btnD.Size = new System.Drawing.Size(180, 72);
+            this.btnD.TabIndex = 13;
+            // 
+            // CampaignFormExt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 520);
+            this.Controls.Add(this.pnlMainNav);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CampaignFormExt";
+            this.Text = "CampaignFormExt";
+            this.pnlMainNav.ResumeLayout(false);
+            this.pnlLib.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel pnlMainNav;
+        private System.Windows.Forms.Panel pnlLib;
+        private Guna.UI2.WinForms.Guna2Button btnD;
+        private Guna.UI2.WinForms.Guna2Button btnB;
+        private Guna.UI2.WinForms.Guna2Button btnC;
+        private Guna.UI2.WinForms.Guna2Button btnA;
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel pnlTab;
+        private System.Windows.Forms.Panel pnlSearching;
+    }
+}
