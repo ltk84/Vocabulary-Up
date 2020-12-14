af7fe19977d7dd44e023e9f2a44e9f9f7f140621
TungTLT <tranlethanhtung554@gmail.com>
2020-12-14 14:56:14 +0700

    Reset

---
+++ VocabularyUp/WalkthroughForm.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace VocabularyUp
+{
+    partial class WalkthroughForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // WalkthroughForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 422);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.Name = "WalkthroughForm";
+            this.Text = "WalkthroughForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
