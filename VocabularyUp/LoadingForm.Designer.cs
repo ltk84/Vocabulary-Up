f4d8b789be129c395b4aabecbf9fc74dbcfec1de
Truong Kim Lam <lamtk.84@gmail.com>
2020-12-21 15:35:42 +0700

    Update loading form from phuoc/update-loadingbar

---
+++ VocabularyUp/LoadingForm.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace VocabularyUp
+{
+    partial class LoadingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoadingForm));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.guna2WinProgressIndicator1 = new Guna.UI2.WinForms.Guna2WinProgressIndicator();
+            this.pbLoading = new Guna.UI2.WinForms.Guna2ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbPercentLoading = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.pbInfo = new Guna.UI2.WinForms.Guna2PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // guna2WinProgressIndicator1
+            // 
+            this.guna2WinProgressIndicator1.AutoStart = true;
+            this.guna2WinProgressIndicator1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.guna2WinProgressIndicator1.CircleSize = 1F;
+            this.guna2WinProgressIndicator1.Location = new System.Drawing.Point(370, 363);
+            this.guna2WinProgressIndicator1.Name = "guna2WinProgressIndicator1";
+            this.guna2WinProgressIndicator1.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(223)))), ((int)(((byte)(158)))));
+            this.guna2WinProgressIndicator1.Size = new System.Drawing.Size(98, 98);
+            this.guna2WinProgressIndicator1.TabIndex = 0;
+            // 
+            // pbLoading
+            // 
+            this.pbLoading.BorderRadius = 4;
+            this.pbLoading.FillColor = System.Drawing.Color.LightGreen;
+            this.pbLoading.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.Horizontal;
+            this.pbLoading.Location = new System.Drawing.Point(101, 316);
+            this.pbLoading.Name = "pbLoading";
+            this.pbLoading.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(223)))), ((int)(((byte)(158)))));
+            this.pbLoading.ProgressColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(223)))), ((int)(((byte)(158)))));
+            this.pbLoading.ShadowDecoration.Parent = this.pbLoading;
+            this.pbLoading.Size = new System.Drawing.Size(644, 10);
+            this.pbLoading.TabIndex = 1;
+            this.pbLoading.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat Alternates", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(730, 66);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Welcome to Vocabulary Up";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat Alternates", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(222, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Please, wait";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Montserrat Alternates", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(395, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(259, 28);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "% more to start learning";
+            // 
+            // lbPercentLoading
+            // 
+            this.lbPercentLoading.AutoSize = true;
+            this.lbPercentLoading.Font = new System.Drawing.Font("Montserrat Alternates", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPercentLoading.Location = new System.Drawing.Point(364, 269);
+            this.lbPercentLoading.Name = "lbPercentLoading";
+            this.lbPercentLoading.Size = new System.Drawing.Size(25, 28);
+            this.lbPercentLoading.TabIndex = 6;
+            this.lbPercentLoading.Text = "0";
+            this.lbPercentLoading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 50;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Location = new System.Drawing.Point(72, 36);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.ShadowDecoration.Parent = this.guna2PictureBox1;
+            this.guna2PictureBox1.Size = new System.Drawing.Size(142, 124);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox1.TabIndex = 2;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // pbInfo
+            // 
+            this.pbInfo.BackColor = System.Drawing.Color.Transparent;
+            this.pbInfo.Image = ((System.Drawing.Image)(resources.GetObject("pbInfo.Image")));
+            this.pbInfo.Location = new System.Drawing.Point(388, 37);
+            this.pbInfo.Name = "pbInfo";
+            this.pbInfo.ShadowDecoration.Parent = this.pbInfo;
+            this.pbInfo.Size = new System.Drawing.Size(90, 109);
+            this.pbInfo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbInfo.TabIndex = 54;
+            this.pbInfo.TabStop = false;
+            // 
+            // LoadingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 500);
+            this.Controls.Add(this.pbInfo);
+            this.Controls.Add(this.lbPercentLoading);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.Controls.Add(this.pbLoading);
+            this.Controls.Add(this.guna2WinProgressIndicator1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoadingForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoadingForm";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private Guna.UI2.WinForms.Guna2WinProgressIndicator guna2WinProgressIndicator1;
+        private Guna.UI2.WinForms.Guna2ProgressBar pbLoading;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbPercentLoading;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2PictureBox pbInfo;
+    }
+}
